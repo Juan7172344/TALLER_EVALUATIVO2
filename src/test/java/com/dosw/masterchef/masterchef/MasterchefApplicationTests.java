e89f617a64211d11823ea60ff7857dc7bd2e6fd3
Juan Andres <Juan.sfonesca@mail.escuelaing.edu.co>
2025-10-21 12:06:10 -0500

    feat: Se sube el spring completo hecho en spring initializr

---
+++ src/test/java/com/dosw/masterchef/masterchef/MasterchefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosw.masterchef.masterchef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterchefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
